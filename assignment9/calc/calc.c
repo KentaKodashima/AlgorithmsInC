ab4c6af613db989de8b4d4cfb227da1616797c86
Kenta Kodashima <kentakodashima@gmail.com>
2018-08-19 23:24:40 -0700

    done with 00 and 09 (asn9)

--- assignment9/calc/calc.c
+++ assignment9/calc/calc.c
@@ -9,59 +9,70 @@
 #include <stdio.h>
 #include "./includes/libex6.h"
 
-int length(char *str)
+typedef int (*operators)(int, int);
+// make an array which has enough spaces to store all ascii table chars
+operators operationSymbols[128] = {NULL};
+
+int add(int a, int b)
 {
-  int counter;
-  while (*str)
-  {
-    counter++;
-    str++;
-  }
-  return counter;
+  return a + b;
 }
 
-void calc(char *val1, char *opt, char *val2)
+int substract(int a, int b)
+{
+  return a - b;
+}
+
+int multiply(int a, int b)
+{
+  return a * b;
+}
+
+int devide(int a, int b)
+{
+  return a / b;
+}
+
+int remaind(int a, int b)
+{
+  return a % b;
+}
+
+// init func pointers array
+void init()
+{
+  operationSymbols['+'] = add;
+  operationSymbols['-'] = substract;
+  operationSymbols['*'] = multiply;
+  operationSymbols['/'] = devide;
+  operationSymbols['%'] = devide;
+}
+
+// check the operator
+int checkOperator(char operator, int val1, int val2)
+{
+  operators operationSymbol;
+  operationSymbol = operationSymbols[operator];
+
+  return operationSymbol(val1, val2);
+}
+
+int calc(char *val1, char *opt, char *val2)
 {
   int result = 0;
   int val1Int = vc_atoi(val1);
   int val2Int = vc_atoi(val2);
 
-  printf("%d\n", val1Int);
+  init();
 
-  if (opt[0] == '+')
-  {
-    result = val1Int + val2Int;
-    // printf("%d\n", result);
-  }
-  else if (opt[0] == '-')
-  {
-    result = val1Int - val2Int;
-  }
-  else if (opt[0] == '*')
-  {
-    result = val1Int * val2Int;
-  }
-  else if (opt[0] == '/')
-  {
-    result = val1Int / val2Int;
-  }
-  else if (opt[0] == '%')
-  {
-    result = val1Int % val2Int;
-  }
-
-  if (length(opt) > 1)
-  {
-    result = 0;
-  }
-
-  vc_putnbr(result);
+  return checkOperator(opt[0], val1Int, val2Int);
 }
 
 int main(int argc, char *argv[])
 {
 
-  calc(argv[0], argv[1], argv[2]);
+  int result = calc(argv[1], argv[2], argv[3]);
+  printf("%d\n", result);
 
   return 0;
 }
--- assignment9/vc_map.c
+++ assignment9/vc_map.c
@@ -1,6 +1,6 @@
 /* ************************************ */
 /*                                      */
-/* vc_map.c                             */  
+/* vc_map.c                             */
 /*                                      */
 /* By: Charles, Emre A, Kenta           */
 /*                                      */
@@ -13,10 +13,10 @@ int test(int num)
   return num * 2;
 }
 
-int *vc_map(int *tab, int length, int(*f)(int))
+int *vc_map(int *tab, int length, int (*f)(int))
 {
   int i = 0;
-  while(i < length)
+  while (i < length)
   {
     tab[i] = (*f)(tab[i]);
     i++;
@@ -27,11 +27,12 @@ int *vc_map(int *tab, int length, int(*f)(int))
 int main(int argc, char const *argv[])
 {
   int arr[3] = {48, 50, 56};
-  int (*p) (int);
+  int (*p)(int);
   p = &test;
   int *tab = vc_map(arr, 3, p);
   printf("%d\n", tab[0]);
   printf("%d\n", tab[1]);
-  
+  printf("%d\n", tab[2]);
+
   return 0;
 }
